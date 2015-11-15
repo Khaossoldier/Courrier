6ccfa3b8f3ccd307c61569993d6118777be168e0
Nicolas DUFLOS <nicolas.duflos@orange.fr>
2015-11-15 06:48:34 +0100

    Fin des test

--- test/mail/RegisterLetterTest.java
+++ test/mail/RegisterLetterTest.java
@@ -2,28 +2,56 @@ package mail;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import city.BankAccount;
+import city.City;
+import city.Inhabitant;
+import content.LetterContent;
 
 public class RegisterLetterTest {
 
+	private int cost;
+	private BankAccount b1;
+	private BankAccount b2;
+	private City c;
+	private Inhabitant monsieur;
+	private Inhabitant madame;
+	private LetterContent content;
+	private Letter<LetterContent> l;
+	
+	@Before
+	public void initTest(){
+		cost = 1;
+		b1 = new BankAccount(5000);
+		b2 = new BankAccount(5000);
+		c = new City("Lille");
+		monsieur = new Inhabitant("Bob", c, b1);
+		madame = new Inhabitant("Gertrude", c, b2);
+		content = new LetterContent(new SimpleLetter(1, monsieur, madame, "text", false));
+		l = new RegisterLetter(cost, monsieur, madame, content);
+	}
+	
 	@Test
 	public void testGetCost() {
-		fail("Not yet implemented");
+		assertEquals(l.getCost(), 16);
 	}
 
 	@Test
 	public void testDescription() {
-		fail("Not yet implemented");
+		assertEquals(l.description(), "a registered letter whose content is " + l.getContent().getDescription());
 	}
 
 	@Test
 	public void testDoAction() {
-		fail("Not yet implemented");
+		l.doAction();
+		assertEquals(b1.getAmount(), 4984);
 	}
 
 	@Test
 	public void testRegisterLetter() {
-		fail("Not yet implemented");
+		assertNotNull(l);
 	}
 
 }
